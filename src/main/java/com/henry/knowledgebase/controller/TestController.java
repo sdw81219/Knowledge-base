9f5b08eb380c961656f5326d361baa57ef8a999c
henry <dawangshen625@gmail.com>
2022-10-12 23:21:47 -0400

    2.7

--- src/main/java/com/henry/knowledgebase/controller/TestController.java
+++ src/main/java/com/henry/knowledgebase/controller/TestController.java
@@ -1,9 +1,10 @@
 package com.henry.knowledgebase.controller;
 
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 @RestController
 public class TestController {
     @GetMapping ("/hello")
