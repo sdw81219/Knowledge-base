b4b0088d3890c73a4073f10dc96dcbbb283964f2
henry <dawangshen625@gmail.com>
2022-10-12 23:39:21 -0400

    2.8.2

--- src/main/java/com/henry/knowledgebase/controller/TestController.java
+++ src/main/java/com/henry/knowledgebase/controller/TestController.java
@@ -1,15 +1,19 @@
 package com.henry.knowledgebase.controller;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
+
+    @Value("${test.hello}")
+    private String testHello;
     @GetMapping ("/hello")
     public String hello(){
-        return "Hello World";
+        return "Hello World"+testHello;
     }
 
 }
