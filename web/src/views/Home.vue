925b749145a6ef4b93cb4598858cdbe398384d11
henry <dawangshen625@gmail.com>
2022-10-15 01:40:44 -0400

    4.5 anti designed vue

---
+++ web/src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <a-button type="primary" shape="round" :size="size">
+      <template #icon>
+        <DownloadOutlined />
+        Download
+      </template>
+    </a-button>
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
+export default defineComponent({
+  name: 'Home',
+  components: {
+    HelloWorld,
+  },
+});
+</script>
