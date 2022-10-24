6fff6308896ea1ca716cd9d73de11bb53d1b020f
henry <dawangshen625@gmail.com>
2022-10-24 13:06:44 -0400

    5.4 list+icon

--- web/src/components/the-footer.vue
+++ web/src/components/the-footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <a-layout-footer style="text-align: center;height: 100px;width: 100%;position: absolute;bottom: 0;left: 0">
+  <a-layout-footer style="text-align: center">
     Henry E-book ©2022 Created by Henry
   </a-layout-footer>
 </template>
@@ -11,3 +11,4 @@ export default defineComponent({
   name: 'the-footer'
 });
 </script>
+
