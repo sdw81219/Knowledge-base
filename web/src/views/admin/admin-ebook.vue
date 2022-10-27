9181d5a0c9f2f296c5ac9058dbf41fbd88c156bf
henry <dawangshen625@gmail.com>
2022-10-27 11:46:41 -0400

    6.3 admin-ebook

---
+++ web/src/views/admin/admin-ebook.vue
@@ -0,0 +1,11 @@
+<template>
+  <a-layout>
+    <a-layout-content
+        :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+    >
+      <div class="about">
+        <h1>Management your Wiki Files</h1>
+      </div>
+    </a-layout-content>
+  </a-layout>
+</template>
