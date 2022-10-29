f6acecc1d79177260d9a7529d98302321e9d1b9d
henry <dawangshen625@gmail.com>
2022-10-29 01:44:47 -0400

    6.8 change api of query ebook

--- src/main/java/com/henry/knowledgebase/controller/EbookController.java
+++ src/main/java/com/henry/knowledgebase/controller/EbookController.java
@@ -1,17 +1,15 @@
 package com.henry.knowledgebase.controller;
 
-import com.henry.knowledgebase.domain.Ebook;
-import com.henry.knowledgebase.req.EbookReq;
+import com.henry.knowledgebase.req.EbookQueryReq;
+import com.henry.knowledgebase.req.EbookSaveReq;
 import com.henry.knowledgebase.resp.CommonResp;
-import com.henry.knowledgebase.resp.EbookResp;
+import com.henry.knowledgebase.resp.EbookQueryResp;
 import com.henry.knowledgebase.resp.PageResp;
 import com.henry.knowledgebase.service.EbookService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 
 import javax.annotation.Resource;
-import java.util.List;
 
 @RestController
 @RequestMapping("/ebook")
@@ -21,10 +19,17 @@ public class EbookController {
     private EbookService ebookService;
 
     @GetMapping("/list")
-    public CommonResp list(EbookReq req) {
-        CommonResp<PageResp<EbookResp>> resp = new CommonResp<>();
-        PageResp<EbookResp> list = ebookService.list(req);
+    public CommonResp list(EbookQueryReq req) {
+        CommonResp<PageResp<EbookQueryResp>> resp = new CommonResp<>();
+        PageResp<EbookQueryResp> list = ebookService.list(req);
         resp.setContent(list);
+        return resp;
+    }
+
+    @PostMapping("/save")
+    public CommonResp save(@RequestBody EbookSaveReq req) {
+        CommonResp resp = new CommonResp<>();
+        ebookService.save(req);
         return resp;
     }
 }
--- src/main/java/com/henry/knowledgebase/req/EbookQueryReq.java
+++ src/main/java/com/henry/knowledgebase/req/EbookQueryReq.java
@@ -1,5 +1,5 @@
 package com.henry.knowledgebase.req;
-public class EbookReq extends PageReq{
+public class EbookQueryReq extends PageReq{
     private Long id;
 
     private String name;
