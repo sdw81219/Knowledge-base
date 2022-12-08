4a17b578ab0087f1888e49127e412ddc09bf91d7
henry <dawangshen625@gmail.com>
2022-12-07 20:34:50 -0600

    8.3 从分类管理复制出文档管理的前后端代码

---
+++ src/main/java/com/henry/knowledgebase/controller/DocController.java
@@ -0,0 +1,51 @@
+package com.henry.knowledgebase.controller;
+
+import com.henry.knowledgebase.req.DocQueryReq;
+import com.henry.knowledgebase.req.DocSaveReq;
+import com.henry.knowledgebase.resp.DocQueryResp;
+import com.henry.knowledgebase.resp.CommonResp;
+import com.henry.knowledgebase.resp.PageResp;
+import com.henry.knowledgebase.service.DocService;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/doc")
+public class DocController {
+
+    @Resource
+    private DocService docService;
+
+    @GetMapping("/all")
+    public CommonResp all() {
+        CommonResp<List<DocQueryResp>> resp = new CommonResp<>();
+        List<DocQueryResp> list = docService.all();
+        resp.setContent(list);
+        return resp;
+    }
+
+    @GetMapping("/list")
+    public CommonResp list(@Valid DocQueryReq req) {
+        CommonResp<PageResp<DocQueryResp>> resp = new CommonResp<>();
+        PageResp<DocQueryResp> list = docService.list(req);
+        resp.setContent(list);
+        return resp;
+    }
+
+    @PostMapping("/save")
+    public CommonResp save(@Valid @RequestBody DocSaveReq req) {
+        CommonResp resp = new CommonResp<>();
+        docService.save(req);
+        return resp;
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public CommonResp delete(@PathVariable Long id) {
+        CommonResp resp = new CommonResp<>();
+        docService.delete(id);
+        return resp;
+    }
+}
